0d50bf5323261136eed4677f65b6712b95a7f2e0
Saulohan <saulolohan09@gmail.com>
2025-04-20 18:04:55 -0300

    Adicionando docker ao projeto

--- TaskManager/TaskManager.API/Program.cs
+++ TaskManager/TaskManager.API/Program.cs
@@ -34,9 +34,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
